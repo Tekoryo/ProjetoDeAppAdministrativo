60d43fc31a2174172ab70d6e6f7ad38f310aaf3c
Thiago <thiagocoelho.coelho2@gmail.com>
2022-01-30 19:55:38 -0300

    iniciando programacao defenciva

---
+++ src/modelo/entities/DomanException.java
@@ -0,0 +1,9 @@
+package modelo.entities;
+
+public class DomanException extends Exception{
+    private static final long serialVersionUID=1149241039409861914L;
+    
+    public DomanException(String msg) {
+        super(msg);
+    }
+}
